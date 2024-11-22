e6533643362765ff9319d2708f710d7885d9d9d6
niraaah <meer@kakao.com>
2024-11-23 00:39:24 +0900

    add ./src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Popular from './pages/Popular';
+import Search from './pages/Search';
+import Wishlist from './pages/Wishlist';
+import SignIn from './pages/SignIn';
+import MovieDetails from './pages/MovieDetails';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/popular" element={<Popular />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
