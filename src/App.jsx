c343d8929a4d2a1e8e5379e8493ef63344fd464b
niraaah <meer@kakao.com>
2024-11-23 15:02:47 +0900

    make github-pages work

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Header from './components/Header';
 const App = () => {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/popular" element={<Popular />} />
